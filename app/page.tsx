fe923c56efcc9b6c2bdc28f023710ed55ac38ec1
Liam McCarthy <ltmccarthy9@gmail.com>
2023-03-08 19:54:37 -0600

    font changes

--- app/page.tsx
+++ app/page.tsx
@@ -13,14 +13,14 @@ export default function Home() {
 
   return (
     <main id='landing' className='flex flex-col'>
-        <div className='h-screen p-6 flex flex-col gap-8 mx-auto mt-20 sm:mt-40 w-full max-w-7xl text-4xl md:text-5xl
+        <div className='h-screen p-6 flex flex-col gap-2 mx-auto mt-20 sm:mt-40 w-full max-w-7xl text-4xl md:text-5xl
          font-bold text-center text-gray-700 dark:text-gray-50'>
-          <h1>Wanna keep in touch with the family?</h1>
-          <h1 className='font-semibold text-2xl md:text-3xl'>But don't want grandma all up in your instagram?</h1>
+          <h1>Wanna keep in touch with your family?</h1>
+          <h1 className=' font-light text-2xl md:text-3xl'>But don't want grandma all up in your instagram?</h1>
           
-          <div className='flex flex-wrap gap-4 mx-auto mt-20 text-4xl md:text-5xl'>
+          <div className='flex flex-wrap gap-4 mx-auto mt-20 text-3xl md:text-4xl'>
           <h1 className='text-gray-700 dark:text-gray-50 font-bold mb-2 mx-auto'>Introducing: </h1>
-          <h1 className='text-indigo-500 dark:text-indigo-400 font-bold mx-auto'>What The Fam Doing?</h1>
+          <h1 className='text-indigo-500 dark:text-indigo-400 font-bold mx-auto mb-4'>What The Fam Doing?</h1>
           </div>
         
           <SignUpButton px='px-20' text='text-lg' m='' display='flex' />
