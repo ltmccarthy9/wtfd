9e9e91354f334e2b6e7a36569ac716501f91c303
Liam McCarthy <ltmccarthy9@gmail.com>
2023-03-11 20:17:45 -0600

    env

--- app/components/NavItems.tsx
+++ app/components/NavItems.tsx
@@ -8,8 +8,8 @@ type NavItemProps = {
 export default function NavItem({ path, title}: NavItemProps){
     return (
         <Link href={`${path}`}>
-            <h2 className="p-2 font-semibold dark:text-gray-50 text-gray-700
-            hover:bg-indigo-400 hover:text-gray-50 rounded-md cursor-pointer ease-in duration-100"
+            <h2 className="p-2 font-normal dark:text-gray-50 text-gray-700
+            hover:bg-indigo-500 hover:text-gray-50 rounded-md cursor-pointer ease-in duration-100"
             >{`${title}`}</h2> 
         </Link>
     )
